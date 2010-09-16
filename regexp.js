0bee8ef38ae7b48a7c173ab035a9eb16593804ee
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2010-09-16 21:12:52 +0200

    added lots of comments and edited the checkbox input routine

--- regexp.js
+++ regexp.js
@@ -1,17 +1,16 @@
-
 /**
  * Updates our regular expression
  */
 function regUpdate() {
 	var mods = new Array();
 	mods['g'] = document.getElementById("regG").checked;
-	mods['i'] = document.getElementById("regG").checked;
-	mods['m'] = document.getElementById("regG").checked;
+	mods['i'] = document.getElementById("regI").checked;
+	mods['m'] = document.getElementById("regM").checked;
 	
 	// get modifiers from our checkboxes
-	var mod = (true ? "g" : "")
-		+ (document.getElementById("regI").checked ? "i" : "")
-		+ (document.getElementById("regM").checked ? "m" : "");
+	var mod = (true ? "g" : "") // this needs some debugging in markMatchedText
+		+ (mods['i'] ? "i" : "")
+		+ (mods['m'] ? "m" : "");
 
 	// output and input DOMs
 	var out = document.getElementById("regOutput");
@@ -38,12 +37,27 @@ function regUpdate() {
 	
 }
 
+/**
+ * Match object, consisting of two variables, result and pos
+ *
+ * @param pos position of the beginning of our match
+ * @param result the matched text
+ */
 function reMatch(pos, res) {
 	this.result = new String(res);
 	this.pos = pos;
 	return this;
 }
 
+/**
+ * matches text from an expression and returns it with markup of where the
+ * expression found matches
+ * 
+ * @param text the input text we wish to match against an expression
+ * @param expression the expression we wish to match against
+ * @param className (optional) a classname that we'll append to the found 
+ * 		matches, within a <span> tag
+ */
 function markMatchedText(text, expression, className) {
 	if(className == null)
 		className = "mark";
@@ -54,9 +68,7 @@ function markMatchedText(text, expression, className) {
 
 	// populate the match array
 	while(validExpressionExec(r = expression.exec(text))) {
-		results.push(new reMatch(
-			(expression.lastIndex-r.toString().length)
-			, r));
+		results.push(new reMatch(expression.lastIndex - r.toString().length, r));
 	}
 	
 	// create a string from the match array
@@ -69,6 +81,12 @@ function markMatchedText(text, expression, className) {
 	return text;
 }
 
+/**
+ * validates that our expression exec() match isn't invalid ("" or null)
+ *
+ * @param exResp response from RegExp.exec() to validate
+ * @return true if expression is valid
+ */
 function validExpressionExec(exResp) {
 	return exResp != null && exResp != "";
 }
